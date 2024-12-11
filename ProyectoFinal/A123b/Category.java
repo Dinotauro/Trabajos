bbc9237dcce38a83ddb760965ee29b274461124e
Dinotauro <98982702+Dinotauro@users.noreply.github.com>
2024-12-11 14:55:50 -0700

    Add files via upload

---
+++ ProyectoFinal/A123b/Category.java
@@ -0,0 +1,17 @@
+package A123b;
+
+import com.google.gson.annotations.JsonAdapter;
+
+import java.time.LocalDateTime;
+
+class Category {
+    public int id;
+    public String name;
+    public String image;
+
+    @JsonAdapter(LocalDateTimeAdapter.class)  // Usamos un adaptador para manejar LocalDateTime
+    public LocalDateTime creationAt;
+
+    @JsonAdapter(LocalDateTimeAdapter.class)
+    public LocalDateTime updatedAt;
+}
